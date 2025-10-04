796d511fb3cd046833124d4be1d529a6f29665c2
Corey Mostero <mostero@berkeley.edu>
2025-10-03 17:57:46 -0700

    feat: regular decisions

--- src/schema/airtable.ts
+++ src/schema/airtable.ts
@@ -13,59 +13,188 @@ export const Status = Schema.Union(
 );
 export type Status = typeof Status.Type;
 
+export const ApplicationEmail = Schema.Literal("Email");
+export const ApplicationFirstName = Schema.Literal("First Name");
+export const ApplicationLastName = Schema.Literal("Last Name");
+export const ApplicationPhone = Schema.Literal("Phone");
+export const ApplicationRole = Schema.Literal("Role");
+export const ApplicationBirthday = Schema.Literal("Birthday");
+export const ApplicationUniversity = Schema.Literal("University");
+export const ApplicationMajorStudying = Schema.Literal("Major / studying");
+export const ApplicationLevelOfStudy = Schema.Literal("Level of study");
+export const ApplicationGraduationClass = Schema.Literal("Graduation class");
+export const ApplicationCountry = Schema.Literal("Country");
+export const ApplicationUnderrepresented = Schema.Literal("Underrepresented?");
+export const ApplicationGender = Schema.Literal("Gender");
+export const ApplicationRaceEthnicity = Schema.Literal("Race / ethnicity");
+export const ApplicationPreviousHackathons = Schema.Literal(
+	"Previous hackathons",
+);
+export const ApplicationLgbtq = Schema.Literal("LGBTQ?");
+export const ApplicationBeenToCalHacksBefore = Schema.Literal(
+	"Been to Cal Hacks before?",
+);
+export const ApplicationPreviousBerkeleyHackathons = Schema.Literal(
+	"Previous Hackathons @ Berkeley events",
+);
+export const ApplicationReferrer = Schema.Literal("Referrer");
+export const ApplicationFavouriteProject = Schema.Literal("Favourite project");
+export const ApplicationPlannedProject = Schema.Literal("Planned project");
+export const ApplicationTakeaways = Schema.Literal("Takeaways");
+export const ApplicationGoal = Schema.Literal("Goal");
+export const ApplicationJoke = Schema.Literal("Joke");
+export const ApplicationGithub = Schema.Literal("GitHub");
+export const ApplicationDevpost = Schema.Literal("Devpost");
+export const ApplicationLinkedIn = Schema.Literal("LinkedIn");
+export const ApplicationResume = Schema.Literal("Resume");
+export const ApplicationEmployer = Schema.Literal("Employer");
+export const ApplicationJobTitle = Schema.Literal("Job title");
+export const ApplicationAreasOfExpertise = Schema.Literal("Areas of expertise");
+export const ApplicationLookingForwardJudge = Schema.Literal(
+	"Looking forward (judge)",
+);
+export const ApplicationPastProjectJudge = Schema.Literal(
+	"Past project (judge)",
+);
+export const ApplicationVolunteerIntroduction = Schema.Literal(
+	"Volunteer introduction",
+);
+export const ApplicationStatus = Schema.Literal("Status");
+export const ApplicationReviewer1 = Schema.Literal("Reviewer 1");
+export const ApplicationReviewer2 = Schema.Literal("Reviewer 2");
+export const ApplicationReviewer3 = Schema.Literal("Reviewer 3");
+export const ApplicationCreatedAt = Schema.Literal("Created at");
+export const ApplicationReviewNeeded = Schema.Literal("Review needed");
+
+export const ApplicationColumns = Schema.Union(
+	ApplicationEmail,
+	ApplicationFirstName,
+	ApplicationLastName,
+	ApplicationPhone,
+	ApplicationRole,
+	ApplicationBirthday,
+	ApplicationUniversity,
+	ApplicationMajorStudying,
+	ApplicationLevelOfStudy,
+	ApplicationGraduationClass,
+	ApplicationCountry,
+	ApplicationUnderrepresented,
+	ApplicationGender,
+	ApplicationRaceEthnicity,
+	ApplicationPreviousHackathons,
+	ApplicationLgbtq,
+	ApplicationBeenToCalHacksBefore,
+	ApplicationPreviousBerkeleyHackathons,
+	ApplicationReferrer,
+	ApplicationFavouriteProject,
+	ApplicationPlannedProject,
+	ApplicationTakeaways,
+	ApplicationGoal,
+	ApplicationJoke,
+	ApplicationGithub,
+	ApplicationDevpost,
+	ApplicationLinkedIn,
+	ApplicationResume,
+	ApplicationEmployer,
+	ApplicationJobTitle,
+	ApplicationAreasOfExpertise,
+	ApplicationLookingForwardJudge,
+	ApplicationPastProjectJudge,
+	ApplicationVolunteerIntroduction,
+	ApplicationStatus,
+	ApplicationReviewer1,
+	ApplicationReviewer2,
+	ApplicationReviewer3,
+	ApplicationCreatedAt,
+	ApplicationReviewNeeded,
+);
+
+export type ApplicationColumns = typeof ApplicationColumns.Type;
+
 export const Application = Schema.Struct({
 	id: Schema.String,
-	email: Schema.optional(Schema.String).pipe(Schema.fromKey("Email")),
-	firstName: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("First Name"),
+	email: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationEmail.literals[0]),
 	),
-	lastName: Schema.optional(Schema.String).pipe(Schema.fromKey("Last Name")),
-	phone: Schema.optional(Schema.String).pipe(Schema.fromKey("Phone")),
-	role: Schema.optional(Schema.String).pipe(Schema.fromKey("Role")),
-	birthday: Schema.optional(Schema.String).pipe(Schema.fromKey("Birthday")),
+	firstName: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationFirstName.literals[0]),
+	),
+	lastName: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationLastName.literals[0]),
+	),
+	phone: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationPhone.literals[0]),
+	),
+	role: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationRole.literals[0]),
+	),
+	birthday: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationBirthday.literals[0]),
+	),
 	university: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("University"),
+		Schema.fromKey(ApplicationUniversity.literals[0]),
 	),
 	majorStudying: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Major / studying"),
+		Schema.fromKey(ApplicationMajorStudying.literals[0]),
 	),
 	levelOfStudy: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Level of study"),
+		Schema.fromKey(ApplicationLevelOfStudy.literals[0]),
 	),
 	graduationClass: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Graduation class"),
+		Schema.fromKey(ApplicationGraduationClass.literals[0]),
 	),
-	country: Schema.optional(Schema.String).pipe(Schema.fromKey("Country")),
+	country: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationCountry.literals[0]),
+	),
 	underrepresented: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Underrepresented?"),
+		Schema.fromKey(ApplicationUnderrepresented.literals[0]),
 	),
-	gender: Schema.optional(Schema.String).pipe(Schema.fromKey("Gender")),
+	gender: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationGender.literals[0]),
+	),
 	raceEthnicity: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Race / ethnicity"),
+		Schema.fromKey(ApplicationRaceEthnicity.literals[0]),
 	),
 	previousHackathons: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Previous hackathons"),
+		Schema.fromKey(ApplicationPreviousHackathons.literals[0]),
 	),
-	lgbtq: Schema.optional(Schema.String).pipe(Schema.fromKey("LGBTQ?")),
+	lgbtq: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationLgbtq.literals[0]),
+	),
 	beenToCalHacks: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Been to Cal Hacks before?"),
+		Schema.fromKey(ApplicationBeenToCalHacksBefore.literals[0]),
 	),
 	previousBerkeleyHackathons: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Previous Hackathons @ Berkeley events"),
+		Schema.fromKey(ApplicationPreviousBerkeleyHackathons.literals[0]),
 	),
-	referrer: Schema.optional(Schema.String).pipe(Schema.fromKey("Referrer")),
+	referrer: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationReferrer.literals[0]),
+	),
 	favouriteProject: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Favourite project"),
+		Schema.fromKey(ApplicationFavouriteProject.literals[0]),
 	),
 	plannedProject: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Planned project"),
+		Schema.fromKey(ApplicationPlannedProject.literals[0]),
 	),
-	takeaways: Schema.optional(Schema.String).pipe(Schema.fromKey("Takeaways")),
-	goal: Schema.optional(Schema.String).pipe(Schema.fromKey("Goal")),
-	joke: Schema.optional(Schema.String).pipe(Schema.fromKey("Joke")),
-	github: Schema.optional(Schema.String).pipe(Schema.fromKey("GitHub")),
-	devpost: Schema.optional(Schema.String).pipe(Schema.fromKey("Devpost")),
-	linkedin: Schema.optional(Schema.String).pipe(Schema.fromKey("LinkedIn")),
+	takeaways: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationTakeaways.literals[0]),
+	),
+	goal: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationGoal.literals[0]),
+	),
+	joke: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationJoke.literals[0]),
+	),
+	github: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationGithub.literals[0]),
+	),
+	devpost: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationDevpost.literals[0]),
+	),
+	linkedin: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationLinkedIn.literals[0]),
+	),
+
 	resume: Schema.optional(
 		Schema.Array(
 			Schema.Struct({
@@ -96,24 +225,44 @@ export const Application = Schema.Struct({
 				),
 			}),
 		),
-	).pipe(Schema.fromKey("Resume")),
-	employer: Schema.optional(Schema.String).pipe(Schema.fromKey("Employer")),
-	jobTitle: Schema.optional(Schema.String).pipe(Schema.fromKey("Job title")),
+	).pipe(Schema.fromKey(ApplicationResume.literals[0])),
+
+	employer: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationEmployer.literals[0]),
+	),
+	jobTitle: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationJobTitle.literals[0]),
+	),
 	areasOfExpertise: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Areas of expertise"),
+		Schema.fromKey(ApplicationAreasOfExpertise.literals[0]),
 	),
 	lookingForwardJudge: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Looking forward (judge)"),
+		Schema.fromKey(ApplicationLookingForwardJudge.literals[0]),
 	),
 	pastProjectJudge: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Past project (judge)"),
+		Schema.fromKey(ApplicationPastProjectJudge.literals[0]),
 	),
 	volunteerIntroduction: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Volunteer introduction"),
+		Schema.fromKey(ApplicationVolunteerIntroduction.literals[0]),
 	),
-	status: Schema.optional(Status).pipe(Schema.fromKey("Status")),
+
+	status: Schema.optional(Status).pipe(
+		Schema.fromKey(ApplicationStatus.literals[0]),
+	),
+	reviewer1: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationReviewer1.literals[0]),
+	),
+	reviewer2: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationReviewer2.literals[0]),
+	),
+	reviewer3: Schema.optional(Schema.String).pipe(
+		Schema.fromKey(ApplicationReviewer3.literals[0]),
+	),
 	createdAt: Schema.optional(Schema.String).pipe(
-		Schema.fromKey("Created at"),
+		Schema.fromKey(ApplicationCreatedAt.literals[0]),
+	),
+	reviewNeeded: Schema.optional(Schema.Number).pipe(
+		Schema.fromKey(ApplicationReviewNeeded.literals[0]),
 	),
 });
 
@@ -121,7 +270,7 @@ export type ApplicationType = typeof Application.Type;
 export type ApplicationEncoded = typeof Application.Encoded;
 
 export const Decision = Schema.Literal("accept", "reject");
-export type Decision = typeof Decision;
+export type Decision = typeof Decision.Type;
 
 export const Review = Schema.Struct({
 	id: Schema.Number,
